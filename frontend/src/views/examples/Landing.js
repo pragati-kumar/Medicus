1e8129339cc1a8ce8fc762ed28d1bec420e634b4
Samarth Arora <54077095+SamarthAroraa@users.noreply.github.com>
2020-11-28 00:17:52 +0530

    converted to functional component

--- frontend/src/views/examples/Landing.js
+++ frontend/src/views/examples/Landing.js
@@ -3,7 +3,7 @@
 =========================================================
 
 */
-import React from "react";
+import React , {useEffect, useRef}from "react";
 // nodejs library that concatenates classes
 import classnames from "classnames";
 
@@ -31,130 +31,131 @@ import CardsFooter from "components/Footers/CardsFooter.js";
 // index page sections
 import Download from "../IndexSections/Download.js";
 
-class Landing extends React.Component {
-  state = {};
-  componentDidMount() {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-    this.refs.main.scrollTop = 0;
-  }
-  render() {
-    return (
-      <>
-        <DemoNavbar />
-        <main ref="main">
-          <div className="position-relative">
-            {/* shape Hero */}
-            <section className="section section-lg section-shaped pb-250">
-              <div className="shape shape-style-1 shape-default">
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-                <span />
-              </div>
-              <Container className="py-lg-md d-flex">
-                <div className="col px-0">
-                  <Row>
-                    <Col lg="6">
-                      <h1 className="display-3 text-white">
-                        Your very own diagnostic tool{" "}
-                      </h1>
-                      <p className="lead text-white">
-                        Just upload a picture of a Chest X-ray to get a
-                        diagnosis from our highly accurate Deep Learning Model
-                      </p>
-                      <div className="btn-wrapper">
+const Landing = (props) => {
+  const main = useRef(0);
+
+    useEffect(()=>{
+      document.documentElement.scrollTop = 0;
+      document.scrollingElement.scrollTop = 0;
+   
+    },[]);
+
+  return (
+    <>
+      <DemoNavbar />
+      <main ref={main}>
+        <div className="position-relative">
+          {/* shape Hero */}
+          <section className="section section-lg section-shaped pb-250">
+            <div className="shape shape-style-1 shape-default">
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+            <Container className="py-lg-md d-flex">
+              <div className="col px-0">
+                <Row>
+                  <Col lg="6">
+                    <h1 className="display-3 text-white">
+                      Your very own diagnostic tool{" "}
+                    </h1>
+                    <p className="lead text-white">
+                      Just upload a picture of a Chest X-ray to get a
+                      diagnosis from our highly accurate Deep Learning Model
+                    </p>
+                    <div className="btn-wrapper">
+                      <Button
+                        className="btn-icon mb-3 mb-sm-0"
+                        color="info"
+                        href="#"
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <i className="ni ni-sound-wave" />
+                        </span>
+                        <span className="btn-inner--text">Get Analysis</span>
+                      </Button>
+                      <Button
+                        className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
+                        color="default"
+                        type="file"
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <i className="ni ni-cloud-upload-96" />
+                        </span>
+                        <span className="btn-inner--text">
+                          Upload an X-ray
+                        </span>
+                      </Button>
+                    </div>
+                  </Col>
+                  <Col lg="6">
+                    <Card className="card-lift--hover shadow border-0">
+                      <CardBody className="py-5">
+                        <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
+                          <i className="ni ni-check-bold" />
+                        </div>
+                        <h6 className="text-primary text-uppercase">
+                          Download Argon
+                        </h6>
+                        <p className="description mt-3">
+                          Argon is a great free UI package based on Bootstrap
+                          4 that includes the most important components and
+                          features.
+                        </p>
+                        <div>
+                          <Badge color="danger" pill className="mr-1">
+                            Viral
+                          </Badge>
+                          <Badge color="danger" pill className="mr-1">
+                            Bacterial
+                          </Badge>
+                          <Badge color="success" pill className="mr-1">
+                            Normal
+                          </Badge>
+                        </div>
                         <Button
-                          className="btn-icon mb-3 mb-sm-0"
-                          color="info"
-                          href="https://demos.creative-tim.com/argon-design-system-react/#/documentation/alerts?ref=adsr-landing-page"
+                          className="mt-4"
+                          color="primary"
+                          href="#pablo"
+                          onClick={(e) => e.preventDefault()}
                         >
-                          <span className="btn-inner--icon mr-1">
-                            <i className="ni ni-sound-wave" />
-                          </span>
-                          <span className="btn-inner--text">Get Analysis</span>
+                          Learn more
                         </Button>
-                        <Button
-                          className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
-                          color="default"
-                          href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"
-                        >
-                          <span className="btn-inner--icon mr-1">
-                            <i className="ni ni-cloud-upload-96" />
-                          </span>
-                          <span className="btn-inner--text">
-                            Upload an X-ray
-                          </span>
-                        </Button>
-                      </div>
-                    </Col>
-                    <Col lg="6">
-                      <Card className="card-lift--hover shadow border-0">
-                        <CardBody className="py-5">
-                          <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
-                            <i className="ni ni-check-bold" />
-                          </div>
-                          <h6 className="text-primary text-uppercase">
-                            Download Argon
-                          </h6>
-                          <p className="description mt-3">
-                            Argon is a great free UI package based on Bootstrap
-                            4 that includes the most important components and
-                            features.
-                          </p>
-                          <div>
-                            <Badge color="primary" pill className="mr-1">
-                              design
-                            </Badge>
-                            <Badge color="primary" pill className="mr-1">
-                              system
-                            </Badge>
-                            <Badge color="primary" pill className="mr-1">
-                              creative
-                            </Badge>
-                          </div>
-                          <Button
-                            className="mt-4"
-                            color="primary"
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            Learn more
-                          </Button>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                  </Row>
-                </div>
-              </Container>
-              {/* SVG separator */}
-              <div className="separator separator-bottom separator-skew">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                  version="1.1"
-                  viewBox="0 0 2560 100"
-                  x="0"
-                  y="0"
-                >
-                  <polygon
-                    className="fill-white"
-                    points="2560 0 2560 100 0 100"
-                  />
-                </svg>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                </Row>
               </div>
-            </section>
-            {/* 1st Hero Variation */}
-          </div>
-        </main>
-      </>
-    );
-  }
+            </Container>
+            {/* SVG separator */}
+            <div className="separator separator-bottom separator-skew">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+          </section>
+          {/* 1st Hero Variation */}
+        </div>
+      </main>
+    </>
+  );
 }
+
 
 export default Landing;
